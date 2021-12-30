753918d2011305979261fc3e4ea6f0ff848bd438
Shaziyaakbar <shaziyaakbar999@gmail.com>
2021-12-30 19:40:40 +0530

    commited

--- Day-5/Pattern_19.java
+++ Day-5/Pattern_19.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
-public class Pattern_18 {
+public class Pattern_19 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++) {
+        char ch='A';
+        for(int i=1;i<=5;i++) {
             for(int j=i;j<=n;j++) {
                 System.out.print(" ");
             }
             for(int j=1;j<=i;j++) {
-                System.out.print(i+ " ");
+                System.out.print(ch+ " ");
             }
             System.out.println();
+            ch++;
         }
     }
 }
+//OUTPUT
+5
+//     A
+//    B B
+//   C C C
+//  D D D D
+// E E E E E
