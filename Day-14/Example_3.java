46e5f20d1921eb090a4732a47e1a8d835514b767
Shaziyaakbar <shaziyaakbar999@gmail.com>
2022-01-27 11:40:23 +0530

    committed

---
+++ Day-14/Example_3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Example_3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String result = str1 +str2;
+        System.out.println(result);
+    }
+}
+//OUTPUT
+//hello 
+//world
+//hello world
